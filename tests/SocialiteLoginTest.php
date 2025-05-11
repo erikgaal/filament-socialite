d4375ac7ca7fe9db247b8e7ff280ab4d51383b52
erikgaal <1234268+erikgaal@users.noreply.github.com>
2025-05-11 19:47:07 +0000

    Fix styling

--- tests/SocialiteLoginTest.php
+++ tests/SocialiteLoginTest.php
@@ -8,7 +8,6 @@ use DutchCodingCompany\FilamentSocialite\Events\RegistrationNotEnabled;
 use DutchCodingCompany\FilamentSocialite\Events\UserNotAllowed;
 use DutchCodingCompany\FilamentSocialite\FilamentSocialitePlugin;
 use DutchCodingCompany\FilamentSocialite\Tests\Fixtures\TestSocialiteUser;
-
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
